1db840db3e161bfe48cc18e9f74d9e2291a5f86d
T1To48 <tofek.98@gmail.com>
2023-03-28 17:33:47 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  //Room State
+  const [room, setRoom] = useState("");
+
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    setMessageHistory([...messageHistory,message])
+    setMessage("")
+    
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+     
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+  return (
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        value={message}
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {messageReceived}
+      {messageHistory?.map((message)=><div key={Math.random()}>{message}</div>)}
+    </div>
+  );
+}
+
+export default App;
